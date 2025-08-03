2dce7d887721a9bd0bccda1edd088e90d6a50850
UPMDNaseer <ss5186066@gmail.com>
2025-08-03 11:01:20 +0530

    ProductDto impl added

--- src/main/java/com/naseer/productmanagement/dto/ProductDto.java
+++ src/main/java/com/naseer/productmanagement/dto/ProductDto.java
@@ -1,6 +1,53 @@
 package com.naseer.productmanagement.dto;
 
 public class ProductDto {
+    private String name;
 
+    private String category;
 
+    private double price;
+
+    private int quantity;
+
+    public ProductDto() {
+    }
+
+    public ProductDto(String name, String category, double price, int quality) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
